a3af4505e21a4d923af06d79404144842d397e88
Todd A Nordquist <toddnordquist144@gmail.com>
2019-03-21 09:56:08 -0600

    Sorting

---
+++ src/edu/cnm/deepdive/MergeSorter.java
@@ -0,0 +1,46 @@
+package edu.cnm.deepdive;
+
+import java.util.Arrays;
+
+public class MergeSorter {
+
+  public void sort(int[] input) {
+    if (input.length > 1) {
+      int midpoint = input.length / 2;
+      // First array will go from 0 (inclusive) to midpoint (exclusive).
+      // Second array will go from midpoint (inclusive) to input.length (exclusive).
+      int[] left = Arrays.copyOfRange(input, 0, midpoint);
+      int[] right = Arrays.copyOfRange(input, midpoint, input.length);
+      sort(left);
+      sort(right);
+      merge(input, left, right);
+    }
+  }
+
+  private void merge(int[] output, int[] left, int[] right) {
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int outputIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        output[outputIndex] = left[leftIndex];
+        leftIndex++;
+      } else {
+        output[outputIndex] = right[rightIndex];
+        rightIndex++;
+      }
+      outputIndex++;
+    }
+    while (leftIndex < left.length) {
+      output[outputIndex] = left[leftIndex];
+      leftIndex++;
+      outputIndex++;
+    }
+    while (rightIndex < right.length) {
+      output[outputIndex] = right[rightIndex];
+      rightIndex++;
+      outputIndex++;
+    }
+  }
+
+}
